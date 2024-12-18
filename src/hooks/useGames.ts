567382e315717ea0387e016ea7b9f9f98eff9889
Jamie Beaman <jamiebeaman@gmail.com>
2024-12-17 18:47:59 -0800

    build sort selector

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -20,7 +20,7 @@ export interface Game {
 const useGames = (gameQuery: GameQuery) => useData<Game>('/games', { 
     params: {
         genres: gameQuery.genre?.id, 
-        platforms: gameQuery.platform?.id 
+        parent_platforms: gameQuery.platform?.id 
     }}, [gameQuery]);
 
 export default useGames;
